39dbef4e0b6b57b2c76e38a96db9bbe02e0b062b
ridhoajibx <ridhoajibx@gmail.com>
2020-05-23 07:08:05 +0700

    create hasRole function to check user role

--- app/Traits/RolePermission.php
+++ app/Traits/RolePermission.php
@@ -7,6 +7,20 @@ use App\Role;
 
 Trait RolePermission
 {
+    /**
+     * users-role (mengecek peran seorang user)
+     */
+    public function hasRole(... $roles)
+    {
+        foreach ($roles as $role) 
+        {
+            if ($this->roles->contains('name', $role)) {
+                return true;
+            }   
+        }
+
+        return false;
+    }
 
     /**
      * Many to many relationship users with roles and permissions table
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    dump(auth()->user()->hasRole('admin'));
 });
 
 Auth::routes();
